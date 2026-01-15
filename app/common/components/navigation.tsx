33a135f1917494539eb0a69881379d5a52064804
iamkw0n <iamkw0n@icloud.com>
2026-01-15 12:54:40 +0900

    #3.2 Navigation part Two

--- app/common/components/navigation.tsx
+++ app/common/components/navigation.tsx
@@ -8,6 +8,8 @@ import {
     NavigationMenuTrigger,
     NavigationMenuContent,
 } from "./ui/navigation-menu";
+import { cn } from "~/lib/utils";
+import { navigationMenuTriggerStyle } from "./ui/navigation-menu";
 
 const menus = [
     {
@@ -132,14 +134,37 @@ export default function Navigation() {
                 <NavigationMenuList>
                     {menus.map((menu) => (
                         <NavigationMenuItem key={menu.name}>
-                            <NavigationMenuTrigger>{menu.name}</NavigationMenuTrigger>
-                            <NavigationMenuContent>
-                                {menu.items?.map((item) => (
-                                    <NavigationMenuItem key={item.name}>
-                                        <Link to={item.to}>{item.name}</Link>
+                            {menu.items ? (<>
+                                <Link to={menu.to}>
+                                    <NavigationMenuTrigger>{menu.name}</NavigationMenuTrigger>
+                                </Link>
+                                    <NavigationMenuContent>
+                                        <ul className="grid w-[600px] font-light gap-3 p-4 grid-cols-2">
+                                            {menu.items?.map((item) => (
+                                                <NavigationMenuItem key={item.name} className={cn([
+                                                    "select-none rounded-md transition-colors hover:bg-accent hover:text-accent-foreground",
+                                                    item.to === "/products/promote" && "col-span-2 bg-primary/10 hover:bg-primary/20 focus:bg-primary/20",
+                                                    item.to === "/jobs/submit" && "col-span-2 bg-primary/10 hover:bg-primary/20 focus:bg-primary/20",
+                                    ])}>
+                                        <NavigationMenuLink asChild>
+                                            <Link
+                                            className="p-3 space-t-1 block leading-none no-underline outline-none"
+                                            to={item.to}>
+                                                <span className="text-sm font-medium leading-none">{item.name}</span>
+                                                <p className="text-sm leading-snug text-muted-foreground">{item.description}</p>
+                                            </Link>
+                                        </NavigationMenuLink>
                                     </NavigationMenuItem>
                                 ))}
+                                </ul>
                             </NavigationMenuContent>
+                            </>
+                            ) : (
+                            <Link 
+                                className={navigationMenuTriggerStyle()}
+                                to={menu.to}>{menu.name}
+                            </Link>
+                        )}
                         </NavigationMenuItem>
                     ))}
                 </NavigationMenuList>
